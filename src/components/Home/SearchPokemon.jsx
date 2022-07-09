8d8bab6a5a955f5f3978c3a1456a949fc237d271
Daniel Espinoza <62059138+DanielEspinoza13a@users.noreply.github.com>
2022-07-09 18:15:27 -0400

    added button for mobile to submit

--- src/components/Home/SearchPokemon.jsx
+++ src/components/Home/SearchPokemon.jsx
@@ -28,9 +28,12 @@ export const SearchPokemon = () => {
     <div>
       <form onSubmit={(e) => getPokeSearch(e)}>
         <div className="relative w-full text-gray-400 focus-within:text-gray-600 border rounded">
-          <div className="absolute inset-y-0 left-0 flex items-center pl-4">
+          <button
+            onClick={() => getPokeSearch()}
+            className="absolute inset-y-0 left-0 flex items-center pl-4 cursor-pointer"
+          >
             <SearchIcon className="h-5 w-5" aria-hidden="true" />
-          </div>
+          </button>
           <input
             id="search-field"
             className=" block w-full h-full pl-12 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
